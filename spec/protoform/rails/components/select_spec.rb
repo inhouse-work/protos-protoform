3e9f38b3564473289637473b1fc9a347aff533a6
Nolan J Tait <nolanjtait@gmail.com>
2025-01-05 23:47:30 -0800

    Adds spec for selecting a date

--- spec/protoform/rails/components/select_spec.rb
+++ spec/protoform/rails/components/select_spec.rb
@@ -21,6 +21,31 @@ RSpec.describe Protoform::Rails::Components::Select, type: :view do
     )
   end
 
+  context "with a date" do
+    it "selects the correct date" do
+      object = double(:object, date_of_birth: Date.new(1990, 1, 2))
+      field = Protoform::Field.new(:date_of_birth, parent: nil, object:)
+
+      render described_class.new(
+        field,
+        collection: [
+          [Date.new(1990, 1, 1), "1990-01-01"],
+          [Date.new(1990, 1, 2), "1990-01-02"]
+        ],
+        include_blank: true,
+        multiple: false
+      )
+
+      expect(page).to have_select(
+        :date_of_birth,
+        selected: "1990-01-02",
+        options: ["", "1990-01-01", "1990-01-02"],
+        id: "date_of_birth",
+        name: "date_of_birth"
+      )
+    end
+  end
+
   it "renders a multiple select input" do
     object = double(:object, preferences: %w[Food Water])
     field = Protoform::Field.new(:preferences, parent: nil, object:)
